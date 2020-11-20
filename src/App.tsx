03f87c38fdce6c97c3a348ca02d03dc80632dadf
mcuteam <echumachenko@avistar.com>
2020-11-20 12:39:49 +0200

    tests added, sass added, ts started

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React, { useState, useCallback, useRef } from 'react';
+import './style.scss'
 
+
+const rowsNum: number = 50;
+const columnsNum: number = 50;
+
+const generateEmptyGrid = () => {
+  const rows: number[][] = [];
+  for (let i = 0; i < rowsNum; i++) {
+    rows.push(Array.from(Array(columnsNum), () => 0));
+  }
+  return rows;
+};
 
 const App: React.FC = () => {
+  const [grid, setGrid] = useState(generateEmptyGrid());
+  // console.log(grid);
   return (
-    <div className="App">
-      some text
+    <div className="app">
+      <div className="grid">
+        {grid.map((rows, i) =>
+          rows.map((col, j) =>
+            <div
+              className='grid-cell'
+              key={`${i}${j}`}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 };
